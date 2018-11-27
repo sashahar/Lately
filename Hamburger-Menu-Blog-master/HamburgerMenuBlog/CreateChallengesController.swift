0501d6ce5ede8410dbbb1d5e349f0f8d86b611fb
Bryce Johnson <brycejohnson@Bryces-MacBook-Pro.local>
2018-11-26 16:15:34 -0800

    Added labels to create challenge

--- Hamburger-Menu-Blog-master/HamburgerMenuBlog/CreateChallengesController.swift
+++ Hamburger-Menu-Blog-master/HamburgerMenuBlog/CreateChallengesController.swift
@@ -4,12 +4,36 @@ import UIKit
 
 class CreateChallengesController: UIViewController {
 
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descriptionField: UITextView!
+    @IBOutlet weak var dateField: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        datePicker.datePickerMode = .date
+        dateField.inputView = datePicker
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneClicked))
+        toolbar.setItems([doneButton], animated: true)
+        
+        dateField.inputAccessoryView = toolbar
     }
     
-    
+    @objc func doneClicked() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        
+        dateField.text = dateFormatter.string(from: datePicker.date)
+        //self.view.endEditing(true)  -- Breaks
+        self.datePicker.endEditing(true)
+    }
 }
 
 
